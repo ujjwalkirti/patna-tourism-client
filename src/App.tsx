247118bc0cb56f89e0471d1be7630098a7c60454
Ujjwal Kirti <64329707+ujjwalkirti@users.noreply.github.com>
2023-06-12 11:38:57 +0530

    Feature: add google based auth feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import "./App.css";
 import MainSection from "./components/MainSection";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   useEffect(() => {
@@ -15,8 +17,9 @@ function App() {
         <span className="text-yellow-300">T</span>ourism{" "}
         <span className="text-yellow-300">API</span> 1.0
       </p>
-      <MainSection/>
-      <Footer/>
+      <MainSection />
+      <Footer />
+      <ToastContainer />
     </div>
   );
 }
--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainSection.css";
 import {
   delete_endpoints,
@@ -7,11 +7,31 @@ import {
   post_endpoints,
 } from "./data";
 import EndPoint from "./Endpoint";
-
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { auth } from "../utils/Firebase";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import { toast } from "react-toastify";
 
 const MainSection = () => {
+  const [user, setUser] = useState<any>({});
+  const [apiKey, setApiKey] = useState<string>("");
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      setUser(user);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
   const question = "font-bold text-3xl text-yellow-300 mb-4";
   const answer = "text-xl ";
 
@@ -62,13 +82,13 @@ const MainSection = () => {
           We have a set of predefined endpoints which you can send requests to.
         </p>
         <p className={answer}>They are listed as follows:</p>
-        <div className="mt-5 w-10/12 mx-auto">
+        <div className="flex flex-col items-center mt-5 w-full lg:w-10/12 mx-auto">
           <p className="mb-4">
             <span className="text-yellow-600 font-semibold">Note :</span> You
             will only be able to access the endpoints if you are attaching your
             API key in your headers, like this:
           </p>
-          <pre className="whitespace-pre-wrap bg-gray-800 border border-gray-700 rounded-md p-4 text-sm font-mono text-gray-200">
+          <pre className="whitespace-pre-wrap bg-gray-800 border border-gray-700 rounded-md p-4 text-sm font-mono text-gray-200 w-full">
             <SyntaxHighlighter
               showLineNumbers
               language="typescript"
@@ -91,12 +111,80 @@ const MainSection = () => {
           `}
             </SyntaxHighlighter>
           </pre>
+          {apiKey.length !== 0 ? (
+            <p className="mt-5">
+              Your{" "}
+              <span className="border-b-4 border-b-yellow-600">API Key</span>{" "}
+              is: {apiKey}
+            </p>
+          ) : (
+            <>
+              <p className="text-left px-2 mt-5 mx-auto rounded-md py-4">
+                If you don't have one,
+                {Object.keys(user).length === 0
+                  ? "You will need to sign in and then generate one."
+                  : "Click here to generate!"}
+              </p>
+              <button
+                onClick={async () => {
+                  const provider = new GoogleAuthProvider();
+                  if (Object.keys(user).length === 0) {
+                    signInWithPopup(auth, provider)
+                      .then((result) => {
+                        fetch("/add-email", {
+                          body: JSON.stringify({
+                            googleId: result.user.uid,
+                            name: result.user.displayName,
+                            email: result.user.email,
+                          }),
+                        });
+                        toast.success(
+                          "Successfully signed in with email " +
+                            result.user.email
+                        );
+                      })
+                      .catch((error) => {
+                        toast.error(error.message);
+                      });
+                  } else {
+                    const body = {
+                      email: user.email,
+                      uid: user.uid,
+                      name: user.displayName,
+                    };
+                    fetch(`${process.env.REACT_APP_BASE_URL_DEV}/get-api-key`, {
+                      method: "POST",
+                      headers: {
+                        "Content-type": "application/json; charset=UTF-8",
+                      },
+                      body: JSON.stringify(body),
+                    })
+                      .then((res) => res.json())
+                      .then((data) => {
+                        setApiKey(data.existingApiKey.key);
+                      })
+                      .catch((error) => {
+                        toast.error(
+                          "Sorry, something went wrong. Please try again."
+                        );
+                      });
+                  }
+                }}
+                className="bg-yellow-600 px-2 py-1 rounded-md"
+              >
+                {Object.keys(user).length !== 0
+                  ? "Generate API Key"
+                  : "Sign In"}
+              </button>
+            </>
+          )}
         </div>
         <p className="mt-5">Also remember,</p>
         <p className="text-center my-5 bg-gray-800 w-4/5 mx-auto rounded-md py-4">
           <span className="text-yellow-600 font-bold ">BASE_URL</span> =
-          "https://patna-tourism-api.azurewebsites.net/"
+          https://patna-tourism-api.azurewebsites.net/
         </p>
+
         <section className="w-11/12 mx-auto mt-5 flex flex-col gap-y-5">
           {/* get endpoints description */}
           <div>
