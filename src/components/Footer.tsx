9db80d2e6a8337fec1c66df2489b3c93ec84d43e
Ujjwal Kirti <64329707+ujjwalkirti@users.noreply.github.com>
2023-06-13 23:44:14 +0530

    Feature: added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Github, Instagram, Link, Linkedin } from 'react-bootstrap-icons';
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div></div>
-  )
+    <div className="py-5 parent">
+      <p className="text-yellow-500">Made with ❤️ by Ujjwal Kirti.</p>
+      <div className="flex items-center justify-center gap-x-5 text-xl socials-container">
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/ujjwal-kirti/"
+        >
+          <Linkedin />
+        </a>
+        {/* Instagram */}
+        <a href="https://www.instagram.com/ujjwal_kirti/">
+          <Instagram />
+        </a>
+        {/* Github */}
+        <a
+          href="https://github.com/ujjwalkirti"
+        >
+          <Github />
+        </a>
+        {/* Portfolio */}
+        <a
+          href="https://www.ujjwalkirti.tech"
+        >
+          <Link />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer
